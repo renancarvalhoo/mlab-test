a19b336a3ba50ca8d937961e76b3e34636a02106
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2017-01-30 00:23:33 -0200

    adjust login from user
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 unless Rails.env.test?
    User.create({:email => "user@mlab.com.br", :password => "mlab@2017", :password_confirmation => "mlab@2017", :name => "User Test"}) unless User.exists?(:email => "user@mlab.com.br")
+   User.create({:email => "renan.azevedo.carvalho@gmail.com", :password => "renan123", :password_confirmation => "renan123", :name => "Renan Carvalho", provider: "email"}) unless User.exists?(:email => "renan.azevedo.carvalho@gmail.com")
   
 end
